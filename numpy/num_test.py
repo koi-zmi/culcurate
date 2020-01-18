85fbdc18565220c7991a3092855a9b00c4ab35cc
koi-zmi <2214053@alumni.tus.ac.jp>
2020-01-19 04:54:11 +0900

    多次元行列の作成

--- numpy/num_test.py
+++ numpy/num_test.py
@@ -1,1 +1,18 @@
+#-*- coding: utf-8 -*-
 import numpy as np
+
+#行数の記入
+horizen = 10
+#列数の記入
+vertical = 8
+
+all_list = []
+i=0
+for i in range(vertical):
+    j=0
+    horizen_list = []
+    for j in range(horizen):
+        horizen_list.append(0)
+    all_list.append(horizen_list)
+    i+= 1
+print(np.array(all_list))
